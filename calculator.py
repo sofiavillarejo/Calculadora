22da052d573eae4eb96e41c4b7fde0c5d7d827d5
Sofia Villarejo <zx22student3329@colegiovalledelmiro.es>
2023-09-26 10:03:14 +0200

    sumar

---
+++ calculator.py
@@ -0,0 +1,12 @@
+#!C:\Users\zx22student3329\AppData\Local\Programs\Python\Python311\python.exe
+
+print("Content-type: text/html\n")
+
+print("Calculadora")
+
+num1 = 2
+num2 = 3
+
+function sumar()
+    print(num1 + num2)
+    
